ced9ebbeffb982d773430e481bdde25e0dfa0c3b
Yossi Zahn <yossizahn@gmail.com>
2022-11-27 13:36:40 +0200

    Get serious... a proper project setup
    Based on: https://mitelman.engineering/blog/python-best-practice/automating-python-best-practices-for-a-new-project/

--- scripts/infix.py
+++ scripts/infix.py
@@ -1,32 +1,39 @@
-from os.path import basename
 import sys
-from fitz import Document
+from os.path import basename
+from typing import Any
+
+from fitz import Document, Page
 
 
-def clean_annotations(page):  
+def clean_annotations(page: Page) -> None:
     for link in page.get_links():
-        if link['uri'] == 'http://www.iceni.com/unlock-pro.htm':
+        if link["uri"] == "http://www.iceni.com/unlock-pro.htm":
             page.delete_link(link)
 
 
-def get_xobjects(pdf, page):
-    def is_water_mark(xobject):
+def get_xobjects(pdf: Document, page: Page) -> list[str]:
+    def is_water_mark(xobject: tuple[int, Any, int, Any]) -> bool:
         (xref, _, invoker, _) = xobject
-        return invoker == 0 and pdf.xref_get_key(xref, 'IceniWatermark') == ('name', '/IceniWatermark')
+        return invoker == 0 and pdf.xref_get_key(xref, "IceniWatermark") == (
+            "name",
+            "/IceniWatermark",
+        )
 
     xobjects = pdf.get_page_xobjects(page.number)
-    return [x[1] for x in xobjects if is_water_mark(x)]  # x[1] holds the object's name
+    return [
+        x[1] for x in xobjects if is_water_mark(x)
+    ]  # x[1] holds the object's name
 
 
-def clean_xobjects(pdf, page, xobjects):
-    def delete_xobject(xobject_name):
+def clean_xobjects(pdf: Document, page: Page, xobjects: list[str]) -> None:
+    def delete_xobject(xobject_name: str) -> None:
         stream: bytes = page.read_contents()
-        while (loc := stream.find(f'/{xobject_name} Do'.encode())) != -1:
-            if (loc_start := stream.rfind(b'q\n', 0, loc)) == -1:
+        while (loc := stream.find(f"/{xobject_name} Do".encode())) != -1:
+            if (loc_start := stream.rfind(b"q\n", 0, loc)) == -1:
                 return
-            if (loc_end := stream.find(b'\nQ', loc)) == -1:
+            if (loc_end := stream.find(b"\nQ", loc)) == -1:
                 return
-            stream = stream[:loc_start] + stream[loc_end + 3:]
+            stream = stream[:loc_start] + stream[loc_end + 3 :]
 
         pdf.update_stream(page.get_contents()[0], stream)
 
@@ -34,25 +41,28 @@ def clean_xobjects(pdf, page, xobjects):
         delete_xobject(xobject_name)
 
 
-def clean_page(pdf, page):
+def clean_page(pdf: Document, page: Page) -> None:
     page.clean_contents()  # concatenates the content streams
     clean_annotations(page)
     xobjects = get_xobjects(pdf, page)
     clean_xobjects(pdf, page, xobjects)
 
 
-def main():
-  if len(sys.argv) < 3:
-      sys.exit(f'Not enough arguments.\n'
-              f'Usage:\n'
-              f'\t{basename(sys.argv[0])} <input.pdf> <output.pdf>')
+def main() -> None:
+    if len(sys.argv) < 3:
+        sys.exit(
+            f"Not enough arguments.\n"
+            f"Usage:\n"
+            f"\t{basename(sys.argv[0])} <input.pdf> <output.pdf>"
+        )
 
-  pdf = Document(sys.argv[1])
+    pdf = Document(sys.argv[1])
 
-  for page in pdf.pages():
-      clean_page(pdf, page)
+    for page in pdf.pages():
+        clean_page(pdf, page)
 
-  pdf.save(sys.argv[2], clean=True, deflate=True)
+    pdf.save(sys.argv[2], clean=True, deflate=True)
+
 
 if __name__ == "__main__":
-  main()
+    main()
--- scripts/pdfxchange.py
+++ scripts/pdfxchange.py
@@ -1,32 +1,38 @@
-from os.path import basename
 import sys
+from os.path import basename
+
 from fitz import Document
 
 
-def clean_annotations():
+def clean_annotations() -> None:
     for link in g_page.get_links():
-        if link['uri'] == 'https://www.tracker-software.com/product/pdf-xchange-editor':
+        if (
+            link["uri"]
+            == "https://www.tracker-software.com/product/pdf-xchange-editor"
+        ):
             g_page.delete_link(link)
 
 
-def get_xobjects():
-    def is_water_mark(xobject):
+def get_xobjects() -> list[str]:
+    def is_water_mark(xobject: tuple[int]) -> bool:
         xref = xobject[0]
-        return g_pdf.xref_is_stream(xref) and b'Click to BUY NOW!' in g_pdf.xref_stream(xref)
+        return g_pdf.xref_is_stream(
+            xref
+        ) and b"Click to BUY NOW!" in g_pdf.xref_stream(xref)
 
     xobjects = g_pdf.get_page_xobjects(g_page.number)
     return [x[1] for x in xobjects if is_water_mark(x)]
 
 
-def clean_xobjects(xobjects):
-    def delete_xobject(xobject_name):
+def clean_xobjects(xobjects: list[str]) -> None:
+    def delete_xobject(xobject_name: str) -> None:
         stream: bytes = g_page.read_contents()
-        while (loc := stream.find(f'/{xobject_name} Do'.encode())) != -1:
-            if (loc_start := stream.rfind(b'/Artifact', 0, loc)) == -1:
+        while (loc := stream.find(f"/{xobject_name} Do".encode())) != -1:
+            if (loc_start := stream.rfind(b"/Artifact", 0, loc)) == -1:
                 return
-            if (loc_end := stream.find(b'EMC', loc)) == -1:
+            if (loc_end := stream.find(b"EMC", loc)) == -1:
                 return
-            stream = stream[:loc_start] + stream[loc_end + 3:]
+            stream = stream[:loc_start] + stream[loc_end + 3 :]
 
         g_pdf.update_stream(g_page.get_contents()[0], stream)
 
@@ -34,7 +40,7 @@ def clean_xobjects(xobjects):
         delete_xobject(xobject_name)
 
 
-def clean_page():
+def clean_page() -> None:
     g_page.clean_contents()  # concatenates the content streams
     clean_annotations()
     xobjects = get_xobjects()
@@ -42,9 +48,11 @@ def clean_page():
 
 
 if len(sys.argv) < 3:
-    sys.exit(f'Not enough arguments.\n'
-             f'Usage:\n'
-             f'\t{basename(sys.argv[0])} <input.pdf> <output.pdf>')
+    sys.exit(
+        f"Not enough arguments.\n"
+        f"Usage:\n"
+        f"\t{basename(sys.argv[0])} <input.pdf> <output.pdf>"
+    )
 
 g_pdf = Document(sys.argv[1])
 
